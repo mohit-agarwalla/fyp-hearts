33e0e4a812b7e6c2447bf7d3742b189d9f892897
Mohit <agarwalla.mohit@yahoo.co.uk>
2023-12-06 16:14:32 +0000

    Add MIT_BH Noise database and load records

---
+++ utils.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import numpy as np
+import wfdb
+import ast
+
+PTB_PATH = "datasets\PTB-XL"
+
+def load_raw_data(df, sampling_rate, path):
+    if sampling_rate == 100:
+        data = [wfdb.rdsamp(path+f) for f in df.filename_lr]
+    else:
+        data = [wfdb.rdsamp(path+f) for f in df.filename_hr]
+    data = np.array([signal for signal, meta in data])
+    return data
+
